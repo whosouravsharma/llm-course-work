79bdb5f8c4cc39915b9c4a14fc275953771666c3
Sourav Sharma <whosouravsharma@gmail.com>
2025-10-05 04:13:18 +0530

    initial commit

---
+++ main.py
@@ -0,0 +1,16 @@
+from dotenv import load_dotenv
+
+load_dotenv()
+
+from langchain import hub
+from langchain.agents import AgentExecutor 
+from langchain.agents.react.agent import create_react_agent
+
+def main():
+    print("Hello from llm-course-work!")
+
+    
+
+
+if __name__ == "__main__":
+    main()
